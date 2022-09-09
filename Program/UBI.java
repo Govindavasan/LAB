6c1a0765474068c097a23a8bc815ddec31479b99
Govindavasan <76154056+Govindavasan@users.noreply.github.com>
2022-09-09 17:53:18 +0530

    Add files via upload

---
+++ Program/UBI.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+ class account
+{
+	double balance;
+	
+	public void credit(double amount)
+	{
+		balance = balance+amount;
+	}
+	
+	public void credit_1(double amount)
+	{
+		balance = balance+amount;
+	}
+	
+	
+	public double getBalance()
+	{
+	
+	return balance;
+	
+	}
+	
+	
+	public void debit(double amount, account anotherAccount)
+	{
+		anotherAccount.credit_1(amount);
+		balance=balance-amount;
+		System.out.println("You want to transfer the amount: "+ amount);
+		System.out.println();
+		System.out.println("---------After Transfer balance amount------------");
+		System.out.println();
+	}
+	}
+	
+	class UBI
+	{
+		public static void main(String[]args)
+		{
+			IDFC obj = new IDFC();
+		    obj.credit(7000);
+			System.out.println();
+			System.out.println("Balance Amount in Account1: "+obj.getBalance());
+			
+			IDFC obj1 = new IDFC();
+			obj1.credit_1(20000);
+			System.out.println("Balance Amount in Account2: "+obj1.getBalance());
+			System.out.println();
+            
+			obj1.debit(4000, obj);
+			
+			System.out.println("After transfer balance amount in Account1: "+obj.getBalance());
+			System.out.println("After transfer balance amount in Account2: "+obj1.getBalance());
+			
+		}
+	}
