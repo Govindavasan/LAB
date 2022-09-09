0b8b458d252243d62442a44c7617768c4b9c4f3f
Govindavasan <76154056+Govindavasan@users.noreply.github.com>
2022-09-09 17:56:33 +0530

    Update UBI.java

--- Program/UBI.java
+++ Program/UBI.java
@@ -1,3 +1,14 @@
+/*
+
+@Author: Govinda Vasan S
+Date   : 09/09/2022
+Title  : A class called Account, which models a bank account of a customer, is designed as shown 
+         in the following class diagram. The methods credit(amount) and debit(amount) add or subtract 
+         the given amount to the balance. The method transferTo(anotherAccount, amount) transfers the given 
+         amount from this Account to the given anotherAccount. Write the Account class.
+	 
+*/
+
 import java.util.*;
 
  class account
